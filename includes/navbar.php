a9f9aa50455db1529ac5104fd76c6223a00004d1
Gabriel Dev <Gabriel@gmail.com>
2025-09-20 19:05:04 -0300

    navbar em amdamento, mas por hora um podelo oficial já foi lançado

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,31 +1,25 @@
-<style>
-  
-</style>
+<nav class="navbar navbar-expand-lg fixed-top navColor">
+  <div class="container">
+    <!-- Logo -->
+    <a class="navbar-brand" href="../pages/index.php">
+      <img src="../assets/img/logo emporio com fundo.jpg" alt="Logo" width="50" class="logoNav">
+    </a>
 
-<nav class="navbar navbar-expand-lg text-body-emphasis fixed-top navColor">
-  <div class="container-fluid">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-4">
-          <div class="container">
-            <a class="navbar-brand logoNav" href="../pages/index.php">
-              <img src="../assets/img/logo emporio com fundo.jpg" alt="Bootstrap" width="50" >
-            </a>
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <ul class="navbar-nav text-center">
-            <li class="nav-item"><a class="nav-link text-light" href="index.php">Início</a></li>
-            <li class="nav-item"><a class="nav-link text-light" href="catalogo.php">Catálogo</a></li>
-            <li class="nav-item"><a class="nav-link text-light" href="promocoes.php">Promoções</a></li>
-            <li class="nav-item"><a class="nav-link text-light" href="unidades.php">Unidades</a></li>
-            <li class="nav-item"><a class="nav-link text-light" href="sobre.php">Sobre</a></li>
-          </ul>
-        </div>
-        <div class="col-sm-4">
-          
-        </div>
-      </div>
+    <!-- Botão Hamburguer (aparece no mobile) -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Links -->
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav text-center">
+        <li class="nav-item"><a class="nav-link text-light" href="index.php">Início</a></li>
+        <li class="nav-item"><a class="nav-link text-light" href="catalogo.php">Catálogo</a></li>
+        <li class="nav-item"><a class="nav-link text-light" href="promocoes.php">Promoções</a></li>
+        <li class="nav-item"><a class="nav-link text-light" href="unidades.php">Unidades</a></li>
+        <li class="nav-item"><a class="nav-link text-light" href="sobre.php">Sobre</a></li>
+      </ul>
     </div>
   </div>
 </nav>
