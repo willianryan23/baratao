dc740a572574b49a51d709edf7a1b2ca3c46db67
willianryan23 <regobemfica@gmail.com>
2025-09-20 00:26:04 -0300

    aicionando listagem produtos

--- admin/catalogo_adm.php
+++ admin/catalogo_adm.php
@@ -1,3 +1,10 @@
 <?php 
+include "../includes/banco.php";
 
+$sql = "SELECT * FROM produtos";
+$result = mysqli_query($conn, $sql);
+
+while($row = mysqli_fetch_assoc($result)) {
+    echo "ID: " . $row["id"] . " - Nome: " . $row["nome"] . " - descrição: " . $row["descricao"] . "<br>";
+}
 ?>
