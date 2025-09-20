eb49e2072234f4c5c6297918308e1237345b6c6b
Gabriel Dev <Gabriel@gmail.com>
2025-09-19 23:16:01 -0300

    ajeitei todos os dados e estrutura da tah de cabeçalho da página e da barra de navegação, tanto pro celular quanto pro notebook

---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<header class="headerBackground">
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+      <!-- Logo -->
+      <a class="navbar-brand" href="#">
+        <img src="../assets/img/logo emporio baratao sem fundo.png" alt="Logo" width="60">
+      </a>
+
+      <!-- Botão Hamburguer (mobile) -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Alternar navegação">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Menu + Busca -->
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <!-- Links de navegação -->
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item"><a class="nav-link active" href="index.php">Início</a></li>
+          <li class="nav-item"><a class="nav-link" href="catalogo.php">Catálogo</a></li>
+          <li class="nav-item"><a class="nav-link" href="promocoes.php">Promoções</a></li>
+          <li class="nav-item"><a class="nav-link" href="unidades.php">Unidades</a></li>
+          <li class="nav-item"><a class="nav-link" href="sobre.php">Sobre</a></li>
+        </ul>
+
+        <!-- Campo de busca -->
+        <form class="d-flex mt-2 mt-lg-0">
+          <input class="form-control me-2" type="search" placeholder="Buscar produtos..." aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Buscar</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
