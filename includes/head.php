bcdc1e5f736596f37bf4035b517cc77a71712580
Gabriel Dev <Gabriel@gmail.com>
2025-09-19 22:40:10 -0300

    adicionado o link de estilização do boostrap

--- includes/head.php
+++ includes/head.php
@@ -9,4 +9,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- link do icone da página -->
     <link rel="shortcut icon" href="../assets/img/icone emporio com fundo.ico" type="image/x-icon">
+    <!-- link das aplicações de estilo do boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
