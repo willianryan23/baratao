d8cf35550aec17bb4189dd5ef0db7553dd2caf93
Gabriel Dev <Gabriel@gmail.com>
2025-09-20 18:52:58 -0300

    carregando as alteações feitas pelo willian

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                Nossa Loja
+            </div>
+            <div class="col-sm-4">
+                Links Uteis
+            </div><div class="col-sm-4">
+                Forma de Contato
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="container text-center">
+            <img src="../assets/img/logo emporio com fundo.jpg" alt="logo Grupo Empório" width="100" class="logoFooter">
+            <p>&copy; 2025 Empório do Baratão. Todos os direitos reservados.</p>
+            
+        </div>
+    </div>
+</footer>
