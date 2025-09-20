e6ed2c869ec87270495ffa189bab4da77433e796
Gabriel Dev <Gabriel@gmail.com>
2025-09-19 22:40:32 -0300

    adcionei o includ do script do boostrap

--- pages/index.php
+++ pages/index.php
@@ -2,9 +2,18 @@
 <html lang="pt-br">
 <!-- include do head -->
 <?php 
-    include "../includes/head.php"
+    include "../includes/head.php";
 ?>
 <body>
-    
+<!-- include do header com a navbar de navegação no site -->
+<?php 
+    include "../includes/header.php";
+?>    
+<h1>teste código</h1>
+
+<!-- script das funcionalidades do boostrap -->
+<?php 
+    include "../includes/scriptBoostrap.php";
+?>
 </body>
 </html>
