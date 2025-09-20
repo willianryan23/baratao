5a3c247e175bc4e7f117a4db266011b55ab3ef73
Gabriel Dev <Gabriel@gmail.com>
2025-09-19 23:47:55 -0300

    código preparado para receber os dados que vão vir do banco

--- pages/index.php
+++ pages/index.php
@@ -40,50 +40,38 @@ include "../includes/head.php";
     </div>
 
     <!-- container flex para os itens do atacado -->
-    <div class="container  border">
+    <div class="container border">
         <!-- container por card de produto -->
         <div class="container">
             <div class="row row-cols-2">
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                <?php
+                // Inclui o arquivo de conexão
+                include "../includes/banco.php";
+
+                // Consulta dos produtos
+                $sql = "SELECT titulo, descricao, imagem FROM produtos LIMIT 4";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        ?>
+                        <div class="col">
+                            <div class="card" style="width: 18rem;">
+                                <img src="<?php echo htmlspecialchars($row['imagem']); ?>" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo htmlspecialchars($row['titulo']); ?></h5>
+                                    <p class="card-text"><?php echo htmlspecialchars($row['descricao']); ?></p>
+                                    <a href="#" class="btn btn-primary">Ver mais</a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
+                        <?php
+                    }
+                } else {
+                    echo "<p>Nenhum produto encontrado.</p>";
+                }
+                $conn->close();
+                ?>
             </div>
         </div>
     </div>
