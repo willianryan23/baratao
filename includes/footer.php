75a91f74feafa8e077b2a793174765c3d5240ca5
Gabriel Dev <Gabriel@gmail.com>
2025-09-21 08:48:23 -0300

    editei pequenas coisas nos textos do footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,8 @@
     
         <div class="container text-center border rounded p-3 bg-body-secondary">
             <img src="../assets/img/logo emporio com fundo.jpg" alt="logo Grupo Empório" width="70" class="logoFooter">
-            <p>&copy; 2025 Empório do Baratão. Todos os direitos reservados.</p>
+            <p>&copy; 2025 Empório Baratão. Todos os direitos reservados.</p>
             
         </div>
-   
+    
 </footer>
