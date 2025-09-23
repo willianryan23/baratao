b1eb1afb6dac1c4eab4f7e4877ccbe6a3dd7f999
willianryan23 <regobemfica@gmail.com>
2025-09-23 12:46:05 -0300

    subindo alteracoes

---
+++ admin/navbar_adm.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navColor">
+  <div class="container">
+    <!-- Logo -->
+    <a class="navbar-brand" href="../pages/index.php">
+      <img src="../assets/img/logo emporio com fundo.jpg" alt="Logo" width="50" class="logoNav">
+    </a>
+
+    <!-- Botão Hamburguer (aparece no mobile) -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Links -->
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav text-center">
+        <li class="nav-item"><a class="nav-link text-light" href="cadastro_adm.php">cadastro</a></li>
+        <li class="nav-item"><a class="nav-link text-light" href="catalogo_adm.php">Catálogo</a></li>
+        <li class="nav-item"><a class="nav-link text-light" href="promocoes_adm.php">promoções</a></li>
+        <li class="nav-item"><a class="nav-link text-light" href="unidades_adm.php">Unidades</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
