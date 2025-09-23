047914cc65e01da80f2e318172ac7b62c8c87fa7
Gabriel Dev <Gabriel@gmail.com>
2025-09-23 10:52:43 -0300

    mexendo no estilo do rodapé

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,12 @@
-<footer>
+<footer class="footerContainer text-center text-lg-start mt-4 gap-3">
+
     <div class="container mb-4">
-    
-        <div class="container text-center border rounded p-3 bg-body-secondary">
+
+        <div class="d-flex flex-column align-items-center p-3">
             <img src="../assets/img/logo emporio com fundo.jpg" alt="logo Grupo Empório" width="70" class="logoFooter">
             <p>&copy; 2025 Empório Baratão. Todos os direitos reservados.</p>
-            
         </div>
-    
+
+    </div>
+
 </footer>
