f56671bed3c36a044a9c3339e68e1dfe9a33da4d
Gabriel Dev <Gabriel@gmail.com>
2025-09-26 12:04:12 -0300

    estilo final do rodapé, e zerando o css padrão

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,36 @@
-<footer class="footerContainer text-center text-lg-start fixed-bottom">
+<footer class=" text-white pt-4 bottom-0" style="background: #114279; min-height: 258px;">
+  <div class="container footerContainer">
+    <div class="row ">
 
-    <div class="container">
+      <div class="col-md-4 mb-3">
+        <h5>Empório Atacadão</h5>
+        <p>Oferecemos os melhores produtos no atacado, com qualidade e preços imbatíveis para seu negócio.</p>
+      </div>
 
-    <div class="row">
-        <div class="col">
-            <img src="../assets/img/logo emporio com fundo.jpg" alt="logo Grupo Empório" width="70" class="logoFooter">
+      <div class="col-md-4 mb-3">
+        <h5>Links úteis</h5>
+        <ul class="list-unstyled">
+          <li><a href="#" class="text-white text-decoration-none">Início</a></li>
+          <li><a href="#" class="text-white text-decoration-none">Produtos</a></li>
+          <li><a href="#" class="text-white text-decoration-none">Contato</a></li>
+          <li><a href="#" class="text-white text-decoration-none">Ajuda</a></li>
+        </ul>
+      </div>
+
+      <div class="col-md-4 mb-3">
+        <h5>Contato</h5>
+        <p>Email: email.com</p>
+        <p>Telefone: (00) 00000-0000</p>
+        <div>
+          <a href="#" class="text-white me-3 redeLink"><i class="fa-brands fa-facebook"></i></a>
+          <a href="#" class="text-white me-3 redeLink"><i class="fa-brands fa-instagram"></i></a>
+          <a href="#" class="text-white me-3 redeLink"><i class="fa-brands fa-whatsapp"></i></a>
         </div>
-        <div class="col">
-            <p>&copy; 2025 Empório Baratão. Todos os direitos reservados.</p>
-        </div>
-    </div>
+      </div>
 
     </div>
-
+    <div class="text-center py-3 border-top border-secondary mt-3">
+      © 2025 Empório Atacadão. Todos os direitos reservados.
+    </div>
+  </div>
 </footer>
