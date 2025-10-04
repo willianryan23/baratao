30ba9bf9abab188e51073d930a16ac052a3a6718
Gabriel Dev <Gabriel@gmail.com>
2025-10-04 12:59:17 -0300

    alterações da navbar

--- admin/navbar_adm.php
+++ admin/navbar_adm.php
@@ -1,17 +1,17 @@
-<nav class="navbar navbar-expand-lg navColor id="NavColorAdm">
+<nav class="navbar navbar-expand-lg fixed-top navColorADM" style="background-color: #d91a3d;">
   <div class="container">
     <!-- Logo -->
     <a class="navbar-brand" href="../pages/index.php">
-      <img src="../assets/img/logo emporio com fundo.jpg" alt="Logo" width="50" class="logoNav">
+      <img src="../assets/img/Empório Atacadista.jpeg" alt="Logo" width="50" class="logoNav">
     </a>
 
-    <!-- Botão Hamburguer (aparece no mobile) -->
+    <!-- Botão Hamburguer -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <!-- Links -->
+    <!-- Links principais -->
     <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
       <ul class="navbar-nav text-center">
         <big><li class="nav-item"><a class="nav-link text-light" href="cadastro_adm.php">Cadastro</a></li></big>
