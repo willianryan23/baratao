a6d9ef461295fcf49991a50084c61c8588f112c8
Gabriel Dev <Gabriel@gmail.com>
2025-10-20 07:57:31 -0300

    modify

--- includes/footer.php
+++ includes/footer.php
@@ -8,22 +8,13 @@
         <p>Oferecemos os melhores produtos no atacado, com qualidade e preços imbatíveis para seu negócio.</p>
       </div>
 
-      <!-- Coluna de links institucionais -->
-      <div class="col-md-4 mb-3">
-        <h5>Links úteis</h5>
-        <ul class="list-unstyled">
-          <li><a href="contato.php" class="text-white text-decoration-none">Contato</a></li>
-          <li><a href="ajuda.php" class="text-white text-decoration-none">Ajuda</a></li>
-          <li><a href="politica.php" class="text-white text-decoration-none">Política de Privacidade</a></li>
-          <li><a href="faq.php" class="text-white text-decoration-none">Perguntas Frequentes</a></li>
-        </ul>
-      </div>
+     
 
       <!-- Coluna de contato -->
-      <div class="col-md-4 mb-3">
+      <div class="col-md-6 mb-3">
         <h5>Fale Conosco</h5>
         <p>Email: <span id="emailCopy">emporioatacadista@emporiobaratao.com.br</span></p>
-        <p>Telefone: (00) 00000-0000</p>
+        <p>Telefone: (86) 99844-9225</p>
         <div>
           <a href="#" class="text-white me-3 redeLink"><i class="fa-brands fa-facebook"></i></a>
           <a href="#" class="text-white me-3 redeLink"><i class="fa-brands fa-instagram"></i></a>
